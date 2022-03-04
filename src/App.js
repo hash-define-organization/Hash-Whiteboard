fc3f89aa860f22bca7e27541c7337081f9c92deb
Shreya Ghosh <sgshreya024@gmail.com>
2022-03-04 08:52:19 +0530

    Preloader Added

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
-import './App.scss';
-import Header from './Components/Header/Header';
-import Main from './Components/Main/Main';
+import "./App.scss";
+import React, { Suspense, lazy } from "react";
+import Preloader from "./Components/Preloader/Preloader";
+const Header = lazy(() => import("./Components/Header/Header"));
+const Main = lazy(() => import("./Components/Main/Main"));
 
 function App() {
-	return (
-		<div className="app">
-			<Header />
-			<Main />
-		</div>
-	);
+  return (
+    <>
+      <Suspense fallback={<Preloader />}>
+        <div className="app">
+          <Header />
+          <Main />
+        </div>
+      </Suspense>
+    </>
+  );
 }
 
 export default App;
---
+++ src/Components/Preloader/Preloader.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Preloader.scss";
+
+const Preloader = () => {
+  return (
+    <div className="preloader">
+      <img src="/hashdefine.jfif" alt="" width="auto" height="auto" />
+    </div>
+  );
+};
+
+export default Preloader;
